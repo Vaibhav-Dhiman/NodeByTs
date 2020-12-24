1bd5dc816590c45d8f1f94a2ba44144b0c7f6d53
vaibhav dhiman <princedhiman70@gmail.com>
2020-12-24 16:00:40 +0530

    api skeleton added

--- NodeSetupAndApi/src/app.ts
+++ NodeSetupAndApi/src/app.ts
@@ -5,8 +5,28 @@ import { requestLoggerMiddleware } from './requestLoggerMiddleware';
 const app = express();
 app.use(cors());
 app.use(bodyparser.json());
-
 // TODO - add middleware here
 app.use(requestLoggerMiddleware);
+
+app.get('/todo', (req: express.Request, res:express.Response, next: express.NextFunction) => {
+    res.json([{id: 1, description: 'Buy beers'}]);
+});
+
+app.post('/todo', (req: express.Request, res:express.Response, next: express.NextFunction) => {
+    console.info(req.body);
+    res.end();
+});
+
+
+app.put('/todo/:id', (req: express.Request, res:express.Response, next: express.NextFunction) => {
+    console.info(req.body);
+    console.info(req.params.id);
+    res.end();
+});
+
+app.delete('/todo/:id', (req: express.Request, res:express.Response, next: express.NextFunction) => {
+    console.info(req.params.id);
+    res.end();
+});
 
 export {app};
