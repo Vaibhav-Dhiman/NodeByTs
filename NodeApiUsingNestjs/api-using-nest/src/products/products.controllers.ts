04c849c4f87a6d59c2f8574035fa9cfba783f3db
vaibhav dhiman <princedhiman70@gmail.com>
2020-12-31 16:04:23 +0530

    find single product

--- NodeApiUsingNestjs/api-using-nest/src/products/products.controllers.ts
+++ NodeApiUsingNestjs/api-using-nest/src/products/products.controllers.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { title } from 'process';
 import { Product } from './product.model';
 import { ProductsService } from './products.service';
@@ -21,6 +21,12 @@ export class ProductsController {
     getAllProducts()  {
         const products = this.productService.getAllProducts();
         return {Product: products};
+    }
+
+    @Get(':id')
+    getProduct(@Param('id') prodId: string) {
+        const product = this.productService.getProduct(prodId);
+        return {Product: product};
     }
 }
 
--- NodeApiUsingNestjs/api-using-nest/src/products/products.service.ts
+++ NodeApiUsingNestjs/api-using-nest/src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,NotFoundException } from '@nestjs/common';
 import { Product } from './product.model';
 
 @Injectable()
@@ -6,7 +6,7 @@ export class ProductsService {
    private products: Product[]= [];
 
     insertProduct(title: string, desc:string, price: number) {
-        const prodId = new Date().toString();
+        const prodId = Math.random().toString();
         const newProduct = new Product(prodId, title, desc, price);
         this.products.push(newProduct);
         return prodId;
@@ -15,5 +15,14 @@ export class ProductsService {
     getAllProducts() {
         const products = [...this.products];
         return products;
+    }
+
+    getProduct(id: string) {
+        const products = [...this.products];
+        const product = products.find((prod) => prod.id === id);
+        if(!product) {
+            throw new NotFoundException('Product not found');
+        }
+        return product;
     }
 }
