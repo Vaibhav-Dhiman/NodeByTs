7e9ac335a101c32d0d6ad5ff80aa4b0378eb4a1d
vaibhav dhiman <princedhiman70@gmail.com>
2021-01-04 12:57:48 +0530

    altas & mongoose setup

--- NodeApiUsingNestjs/api-using-nest/src/app.module.ts
+++ NodeApiUsingNestjs/api-using-nest/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/product.module';
 import { ProductsController } from './products/products.controllers';
 import { ProductsService } from './products/products.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot('mongodb+srv://vaibhav:E1Vsz4A1ATrq3wDO@cluster0.4ttpq.mongodb.net/nestjs-demo?retryWrites=true&w=majority')],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
